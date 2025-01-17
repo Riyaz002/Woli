49e1aeafb12d04f0128e5bb97ef9fca75a7a7ba9
Riyaz002 <riyazps007@gmail.com>
2025-01-17 17:52:40 +0530

    updated firebase upload script

--- app/src/main/java/script/UploadImageToFirebase.kt
+++ app/src/main/java/script/UploadImageToFirebase.kt
@@ -1,22 +1,30 @@
-package com.wiseowl.woli.data.remote.script
+package script
 
 import android.content.Context
-import android.util.Log
-import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
 import com.wiseowl.woli.data.local.entity.ImageDTO
 import com.wiseowl.woli.data.remote.api.FirebaseDataService.Companion.IMAGES_DOCUMENT
-import kotlinx.coroutines.runBlocking
-import org.junit.Test
 
 class UploadImageToFirebase {
-    @Test
-    fun upload() = runBlocking {
-        startUploading(getApplicationContext(), "1")
+    fun startUploadingImages(applicationContext: Context, page: Int) {
+        applicationContext.assets.open("imagedata.csv").bufferedReader().use {
+            it.lines().forEach { line ->
+                val (url, description, category) = line.split(",")
+                Firebase.firestore.collection(IMAGES_DOCUMENT).document(url.hashCode().toString()).set(
+                    ImageDTO(
+                        id = url.hashCode(),
+                        url = url,
+                        description = description,
+                        category = category
+                    )
+                )
+            }
+            uploadPage(applicationContext, page)
+        }
     }
 
-    private fun startUploading(applicationContext: Context, documentName: String) {
+    private fun uploadPage(applicationContext: Context, page: Int){
         applicationContext.assets.open("imagedata.csv").bufferedReader().use {
             val images = arrayListOf<ImageDTO>()
             it.lines().forEach { line ->
@@ -31,8 +39,7 @@ class UploadImageToFirebase {
                 )
             }
             if (images.isNotEmpty()) {
-                Log.e("TEST", images.toString())
-                Firebase.firestore.collection(IMAGES_DOCUMENT).document(documentName).set(
+                Firebase.firestore.collection("pages").document(page.toString()).set(
                     mapOf("data" to images)
                 )
             }
