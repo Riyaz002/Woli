d183f41aa2816a21686d636861fe855cd65c2a0c
Riyaz002 <riyazps007@gmail.com>
2025-01-19 23:09:28 +0530

    Added navigation from Home to Detail

--- app/src/main/java/com/wiseowl/woli/ui/screen/home/HomeViewModel.kt
+++ app/src/main/java/com/wiseowl/woli/ui/screen/home/HomeViewModel.kt
@@ -3,8 +3,11 @@ package com.wiseowl.woli.ui.screen.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.wiseowl.woli.configuration.coroutine.Dispatcher
+import com.wiseowl.woli.domain.event.Event
+import com.wiseowl.woli.domain.event.EventHandler
 import com.wiseowl.woli.ui.screen.home.model.HomeState
 import com.wiseowl.woli.domain.usecase.home.HomeUseCase
+import com.wiseowl.woli.ui.navigation.Screen
 import com.wiseowl.woli.ui.screen.home.model.HomePageModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -26,7 +29,7 @@ class HomeViewModel(private val homeUseCase: HomeUseCase): ViewModel() {
     fun onEvent(event: HomeEvent){
         when(event){
             is HomeEvent.OnClickImage -> {
-                //Navigate to detail page
+                EventHandler.sendEvent(Event.Navigate(Screen.DETAIL, mapOf(Screen.DETAIL.ARG_IMAGE_ID to event.imageId.toString())))
             }
             is HomeEvent.LoadNextPage -> loadNextPage(event.page)
         }
