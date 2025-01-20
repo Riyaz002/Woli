053782bc417ceda1ef4ae028784ae95563b4f477
Riyaz002 <riyazps007@gmail.com>
2025-01-20 16:45:50 +0530

    Fixed inner padding

--- app/src/main/java/com/wiseowl/woli/MainActivity.kt
+++ app/src/main/java/com/wiseowl/woli/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
@@ -30,7 +29,8 @@ class MainActivity : ComponentActivity() {
             WoliTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Root(
-                        modifier = Modifier.padding(innerPadding),
+                        modifier = Modifier,
+                        innerPaddingValues = innerPadding,
                         navController = navController
                     )
                 }
--- app/src/main/java/com/wiseowl/woli/ui/navigation/Root.kt
+++ app/src/main/java/com/wiseowl/woli/ui/navigation/Root.kt
@@ -1,8 +1,9 @@
 package com.wiseowl.woli.ui.navigation
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -14,16 +15,17 @@ import com.wiseowl.woli.ui.screen.home.Home
 @Composable
 fun Root(
     modifier: Modifier,
+    innerPaddingValues: PaddingValues,
     navController: NavHostController,
     startScreen: String = Screen.HOME.route
 ) {
     NavHost(modifier = modifier, navController = navController, startDestination = startScreen) {
-        composable(route = Screen.HOME.route) { Home() }
+        composable(route = Screen.HOME.route) { Home(modifier.padding(innerPaddingValues)) }
         composable(
             route = Screen.DETAIL.route,
             arguments = listOf(
                 navArgument(Screen.DETAIL.ARG_IMAGE_ID) { type = NavType.StringType }
             )
-        ) { Detail(imageId = it.arguments!!.getString(Screen.DETAIL.ARG_IMAGE_ID)!!) }
+        ) { Detail(modifier, imageId = it.arguments!!.getString(Screen.DETAIL.ARG_IMAGE_ID)!!) }
     }
 }
