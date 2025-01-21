767d643f6624e14eb7d7f1ab8ff9092861838a87
Riyaz002 <riyazps007@gmail.com>
2025-01-21 22:36:13 +0530

    Updated page/image upload script

--- app/src/main/java/script/UploadImageToFirebase.kt
+++ app/src/main/java/script/UploadImageToFirebase.kt
@@ -6,9 +6,11 @@ import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.firestore
 import com.wiseowl.woli.data.local.entity.ImageDTO
 import com.wiseowl.woli.data.remote.FirebaseDataService.Companion.IMAGES_DOCUMENT
+import com.wiseowl.woli.data.remote.FirebaseDataService.Companion.PAGES_DOCUMENT
+import kotlinx.coroutines.tasks.await
 
 class UploadImageToFirebase {
-    fun startUploadingImages(applicationContext: Context) {
+    suspend fun startUploadingImages(applicationContext: Context) {
         applicationContext.assets.open("imagedata.csv").bufferedReader().use {
             it.lines().forEach { line ->
                 val (url, description, category) = line.split(",")
@@ -21,11 +23,13 @@ class UploadImageToFirebase {
                     )
                 )
             }
-            uploadPage(applicationContext, 2)
+            uploadPage(applicationContext)
         }
     }
 
-    private fun uploadPage(applicationContext: Context, page: Int){
+    private suspend fun uploadPage(applicationContext: Context){
+        val currentPageNo = Firebase.firestore.collection(PAGES_DOCUMENT).document(COUNT).get().await().data?.getValue(TOTAL_PAGE) as Long
+        val page = currentPageNo+1
         applicationContext.assets.open("imagedata.csv").bufferedReader().use {
             val imagesRef = arrayListOf<DocumentReference>()
             it.lines().forEach { line ->
@@ -35,10 +39,16 @@ class UploadImageToFirebase {
                 )
             }
             if (imagesRef.isNotEmpty()) {
-                Firebase.firestore.collection("pages").document(page.toString()).set(
+                Firebase.firestore.collection(PAGES_DOCUMENT).document(page.toString()).set(
                     mapOf("data" to imagesRef)
                 )
             }
+            Firebase.firestore.collection(PAGES_DOCUMENT).document(COUNT).set(mapOf(TOTAL_PAGE to page))
         }
+    }
+
+    companion object{
+        const val COUNT = "count"
+        const val TOTAL_PAGE = "totalPages"
     }
 }
