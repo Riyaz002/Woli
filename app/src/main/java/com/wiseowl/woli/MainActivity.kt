c726b36d201b1facd3cd19916e23329cc54760a0
Riyaz002 <riyazps007@gmail.com>
2025-02-15 19:50:20 +0530

    Fixed padding issues

--- app/src/main/java/com/wiseowl/woli/MainActivity.kt
+++ app/src/main/java/com/wiseowl/woli/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -36,10 +37,9 @@ class MainActivity : ComponentActivity() {
                 }
             }
             WoliTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize()) { it
                     Root(
-                        modifier = Modifier,
-                        innerPaddingValues = innerPadding,
+                        modifier = Modifier.safeContentPadding(),
                         navController = navController
                     )
                     if(progressVisible) CircularProgressBar(Modifier.fillMaxSize())
