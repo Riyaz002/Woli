53abcb10abc8516da0bb472561b5fa63f72d3213
Riyaz002 <riyazps007@gmail.com>
2025-02-28 16:36:51 +0530

    removed extra padding

--- app/src/main/java/com/wiseowl/woli/MainActivity.kt
+++ app/src/main/java/com/wiseowl/woli/MainActivity.kt
@@ -53,7 +53,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize()
                 ) { it
                     Box(
-                        Modifier.safeContentPadding()
+                        Modifier
                     ) {
                         Root(
                             modifier = Modifier.padding(bottom = 28.dp),
