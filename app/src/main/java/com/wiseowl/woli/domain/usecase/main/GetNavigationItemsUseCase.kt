24adff0437384b35be532ef2cbb59eae07aef38e
Riyaz002 <riyazps007@gmail.com>
2025-03-08 21:44:19 +0530

    Category Navigation bar icon

--- app/src/main/java/com/wiseowl/woli/domain/usecase/main/GetNavigationItemsUseCase.kt
+++ app/src/main/java/com/wiseowl/woli/domain/usecase/main/GetNavigationItemsUseCase.kt
@@ -2,6 +2,7 @@ package com.wiseowl.woli.domain.usecase.main
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
 import com.wiseowl.woli.domain.event.Action
 import com.wiseowl.woli.domain.model.NavigationItem
 import com.wiseowl.woli.ui.navigation.Screen
@@ -10,7 +11,7 @@ class GetNavigationItemsUseCase {
     operator fun invoke(): List<NavigationItem>{
         return listOf(
             NavigationItem("Home", Icons.Default.Home, Action.Navigate(Screen.HOME)),
-            NavigationItem("Categories", Icons.Default.Home, Action.Navigate(Screen.CATEGORIES))
+            NavigationItem("Categories", Icons.Default.Menu, Action.Navigate(Screen.CATEGORIES))
         )
     }
 }
