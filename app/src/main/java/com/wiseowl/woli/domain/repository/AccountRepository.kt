2eb6599337e9864be18e1841cf902757c4ba6d86
Riyaz002 <riyazps007@gmail.com>
2025-03-24 10:59:56 +0530

    Code refactor

--- app/src/main/java/com/wiseowl/woli/domain/repository/AccountRepository.kt
+++ app/src/main/java/com/wiseowl/woli/domain/repository/AccountRepository.kt
@@ -4,7 +4,7 @@ import com.wiseowl.woli.domain.model.User
 
 interface AccountRepository {
     suspend fun createAccount(email: String, password: String, firstName: String, lastName: String)
-    suspend fun deleteUser(userId: String)
+    suspend fun deleteUser(email: String)
     suspend fun updateUser(user: User)
     suspend fun isEmailRegistered(email: String): Boolean
     suspend fun getUser(email: String): User?
