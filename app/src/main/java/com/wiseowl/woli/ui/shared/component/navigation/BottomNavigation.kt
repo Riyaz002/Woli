78cccff1a397741c3fe8c961676e5a388b12f232
Riyaz002 <riyazps007@gmail.com>
2025-03-27 23:24:32 +0530

    Added animation

--- app/src/main/java/com/wiseowl/woli/ui/shared/component/navigation/BottomNavigation.kt
+++ app/src/main/java/com/wiseowl/woli/ui/shared/component/navigation/BottomNavigation.kt
@@ -1,5 +1,6 @@
 package com.wiseowl.woli.ui.shared.component.navigation
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -13,11 +14,17 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,32 +39,57 @@ fun BottomNavigation(
     modifier: Modifier = Modifier,
     navigationItems: List<NavigationItem> = GetNavigationItemsUseCase().invoke()
 ) {
+    val itemSize = remember { mutableStateOf(0) }
+    val ballAnimation =
+        animateFloatAsState(targetValue = (itemSize.value.toFloat() / 2) * (0.5 * (-2 + (navigationItems
+            .indexOfFirst { it.selected }
+            .plus(1) * 4))).toFloat()
+        )
     Row(
         modifier
             .clip(RoundedCornerShape(100.dp))
-            .background(Color.Black),
+            .background(Color.White)
+            .drawBehind {
+                drawIntoCanvas {
+                    drawCircle(
+                        Color.Black,
+                        (size.minDimension / 2.0f) - 10,
+                        center = Offset(
+                            x = ballAnimation.value,
+                            y = size.height / 2
+                        )
+                    )
+                }
+            },
         verticalAlignment = Alignment.CenterVertically
     ) {
-        navigationItems.forEach {
+        navigationItems.forEach { navigationItem ->
             Column(
                 Modifier
-                    .background(Color.White)
+                    .onSizeChanged {
+                        itemSize.value = it.width
+                    }
                     .padding(5.dp)
-                    .clickable { ActionHandler.perform(it.action) },
+                    .clickable { ActionHandler.perform(navigationItem.action) },
                 horizontalAlignment = Alignment.CenterHorizontally) {
                 Column(
                     modifier = Modifier
                         .width(80.dp)
                         .aspectRatio(1f)
                         .clip(RoundedCornerShape(100.dp))
-                        .background(if(it.selected) Color.Black else Color.White)
                         .padding(5.dp),
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.Center,
                 ) {
-                    val tintColor = if (it.selected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onSurface
-                    Image(modifier = Modifier.size(24.dp), imageVector = it.icon, contentDescription = it.title, colorFilter = ColorFilter.tint(tintColor))
-                    Text(text = it.title, color = tintColor, fontSize = 11.sp)
+                    val tintColor =
+                        if (navigationItem.selected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onSurface
+                    Image(
+                        modifier = Modifier.size(24.dp),
+                        imageVector = navigationItem.icon,
+                        contentDescription = navigationItem.title,
+                        colorFilter = ColorFilter.tint(tintColor)
+                    )
+                    Text(text = navigationItem.title, color = tintColor, fontSize = 11.sp)
                 }
             }
         }
